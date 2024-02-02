fb9f15d8e91c5e5cee00355dd2e952c086b8f248
odjhey <odjhey@gmail.com>
2024-02-02 16:16:57 +0800

    refactor: move components to own files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { useEffect, useState } from 'react'
-import dayjs from 'dayjs'
-
 import './App.css'
-import useSet from './hooks/use-set'
 import { Hours } from './components/Hours'
 import {
   Link,
@@ -10,6 +6,7 @@ import {
   RouterProvider,
   createBrowserRouter,
 } from 'react-router-dom'
+import { DateDiff } from './components/DateDiff'
 
 const Root = () => {
   return (
@@ -52,108 +49,6 @@ const router = createBrowserRouter([
     ],
   },
 ])
-
-const DatePicker = (props: {
-  selected: Date
-  onChange: (date: Date) => void
-}) => {
-  return (
-    <input
-      type="date"
-      value={props.selected.toISOString().slice(0, 10)}
-      onChange={(e) => props.onChange(new Date(e.target.value))}
-    />
-  )
-}
-
-function getDateArray(from: Date, to: Date) {
-  const fromDate = dayjs(from)
-  const toDate = dayjs(to)
-  const diffDays = toDate.diff(fromDate, 'day')
-
-  return Array.from({ length: diffDays + 1 }, (_, index) =>
-    fromDate.add(index, 'day').toDate(),
-  )
-}
-
-function DateDiff() {
-  const [startDate, setStartDate] = useState<Date>(new Date())
-  const [endDate, setEndDate] = useState<Date>(new Date())
-  const [diff, setDiff] = useState(0)
-  const [selectedDates, selectedDateActions] = useSet(new Set<string>())
-
-  // we lazy for now, so we use useEffect
-  useEffect(() => {
-    if (startDate && endDate) {
-      setDiff(dayjs(endDate).diff(dayjs(startDate), 'day'))
-    }
-  }, [startDate, endDate])
-
-  return (
-    <>
-      <div className="container mx-auto">
-        <div className="flex gap-2">
-          <DatePicker
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-          />
-          -
-          <DatePicker
-            selected={endDate}
-            onChange={(date) => setEndDate(date)}
-          />
-        </div>
-        <div>diff: {diff}</div>
-      </div>
-
-      <div className="flex gap-2 flex-wrap">
-        {getDateArray(startDate, endDate).map((date) => {
-          const badgeClass = selectedDateActions.has(date.toISOString())
-            ? 'badge-accent'
-            : 'badge-neutral'
-
-          return (
-            <div
-              key={date.toISOString()}
-              className={`badge ${badgeClass}`}
-              onClick={() => {
-                if (selectedDateActions.has(date.toISOString())) {
-                  selectedDateActions.remove(date.toISOString())
-                } else {
-                  selectedDateActions.add(date.toISOString())
-                }
-              }}
-            >
-              {dayjs(date).format('YY-MM-DD ddd')}
-            </div>
-          )
-        })}
-      </div>
-
-      <div className="divider"></div>
-      {JSON.stringify(selectedDates.size)}
-      <div className="flex flex-wrap gap-2">
-        {[...selectedDates.values()].map((date) => {
-          return (
-            <div
-              key={date}
-              className={`badge badge-neutral`}
-              onClick={() => {
-                if (selectedDateActions.has(date)) {
-                  selectedDateActions.remove(date)
-                } else {
-                  selectedDateActions.add(date)
-                }
-              }}
-            >
-              {dayjs(date).format('YY-MM-DD ddd')}
-            </div>
-          )
-        })}
-      </div>
-    </>
-  )
-}
 
 const Shell = () => <RouterProvider router={router} />
 
---
+++ src/components/DateDiff.tsx
@@ -0,0 +1,105 @@
+import dayjs from 'dayjs'
+import { useEffect, useState } from 'react'
+import useSet from '../hooks/use-set'
+
+const DatePicker = (props: {
+  selected: Date
+  onChange: (date: Date) => void
+}) => {
+  return (
+    <input
+      type="date"
+      value={props.selected.toISOString().slice(0, 10)}
+      onChange={(e) => props.onChange(new Date(e.target.value))}
+    />
+  )
+}
+
+function getDateArray(from: Date, to: Date) {
+  const fromDate = dayjs(from)
+  const toDate = dayjs(to)
+  const diffDays = toDate.diff(fromDate, 'day')
+
+  return Array.from({ length: diffDays + 1 }, (_, index) =>
+    fromDate.add(index, 'day').toDate(),
+  )
+}
+
+export function DateDiff() {
+  const [startDate, setStartDate] = useState<Date>(new Date())
+  const [endDate, setEndDate] = useState<Date>(new Date())
+  const [diff, setDiff] = useState(0)
+  const [selectedDates, selectedDateActions] = useSet(new Set<string>())
+
+  // we lazy for now, so we use useEffect
+  useEffect(() => {
+    if (startDate && endDate) {
+      setDiff(dayjs(endDate).diff(dayjs(startDate), 'day'))
+    }
+  }, [startDate, endDate])
+
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="flex gap-2">
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
+          -
+          <DatePicker
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+          />
+        </div>
+        <div>diff: {diff}</div>
+      </div>
+
+      <div className="flex gap-2 flex-wrap">
+        {getDateArray(startDate, endDate).map((date) => {
+          const badgeClass = selectedDateActions.has(date.toISOString())
+            ? 'badge-accent'
+            : 'badge-neutral'
+
+          return (
+            <div
+              key={date.toISOString()}
+              className={`badge ${badgeClass}`}
+              onClick={() => {
+                if (selectedDateActions.has(date.toISOString())) {
+                  selectedDateActions.remove(date.toISOString())
+                } else {
+                  selectedDateActions.add(date.toISOString())
+                }
+              }}
+            >
+              {dayjs(date).format('YY-MM-DD ddd')}
+            </div>
+          )
+        })}
+      </div>
+
+      <div className="divider"></div>
+      {JSON.stringify(selectedDates.size)}
+      <div className="flex flex-wrap gap-2">
+        {[...selectedDates.values()].map((date) => {
+          return (
+            <div
+              key={date}
+              className={`badge badge-neutral`}
+              onClick={() => {
+                if (selectedDateActions.has(date)) {
+                  selectedDateActions.remove(date)
+                } else {
+                  selectedDateActions.add(date)
+                }
+              }}
+            >
+              {dayjs(date).format('YY-MM-DD ddd')}
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
